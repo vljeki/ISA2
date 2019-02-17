5e6b93b3f4518db1756e072eb3d0c90adacb4d1f
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-17 20:26:06 +0200

    Work with dates

---
+++ Week3/dates.js
@@ -0,0 +1,12 @@
+let today = new Date();
+
+let bob = new Date('December 7, 1969 07:01:23');
+/* let bob = new Data('1969-12-07T07:01:23');
+let bob = new Data(1969, 11, 6);
+let bob = new Data(1969, 11, 6, 7, 1, 23); */
+
+/* var elapsedTime = today - bob;
+console.log(elapsedTime); */
+
+console.log(bob.getDate()); //Monday = 1, Sunday = 7
+console.log(bob.getTime());
