af8ad5d9c36e56db8e59851253e605e83abac333
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-17 15:20:36 +0200

    Learn how use keyword NEW in constructors

---
+++ Week3/constructors.js
@@ -0,0 +1,17 @@
+/* function Car(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+let myCar = new Car('bmw', '745li', 2010);
+
+console.log(myCar); */
+
+function MyFunction() {
+    console.log('I am a simple function');
+}
+
+let myFunction = new MyFunction();
+
+console.log(typeof myFunction);
