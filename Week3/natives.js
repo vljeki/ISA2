1aac30067cc7111e7cac69046c837742a00136b6
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-17 14:43:01 +0200

    Learn that is built-in natives

---
+++ Week3/natives.js
@@ -0,0 +1,19 @@
+/* let pattern = /xyz/;
+let value = 'This is xyz a test';
+console.log(value.replace(pattern, 'just')); */
+
+//let myString = new String('howdy');
+//console.log(myString);
+//console.log(myString.toString());
+
+//console.log(typeof myString);
+
+/* let myPrimitive = 'THIS IS CRAZY';
+console.log(typeof myPrimitive);
+myPrimitive = myPrimitive.toLocaleLowerCase();
+console.log(typeof myPrimitive); */
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+let myPrimitive = myNumber.valueOf();
+console.log(typeof myPrimitive);
