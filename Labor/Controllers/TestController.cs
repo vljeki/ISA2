f160020b77284dbcf7c2aebc4f653573647ce698
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-21 20:20:40 +0200

    Creat Labor3

--- Labor/Controllers/TestController.cs
+++ Labor/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Labor.Models;
 
 namespace Labor.Controllers
 {
@@ -15,6 +16,12 @@ namespace Labor.Controllers
 
         public ActionResult GetView()
         {
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+
+            ViewData["Employee"] = emp;
             return View("MyView");
         }
     }
---
+++ Labor/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labor.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+    }
+}
