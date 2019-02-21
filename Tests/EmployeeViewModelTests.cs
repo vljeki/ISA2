6b376beafbb131416f214919dba765606a8fbb65
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-21 23:32:59 +0200

    Kirjutasin testid

--- Tests/EmployeeViewModelTests.cs
+++ Tests/EmployeeViewModelTests.cs
@@ -15,6 +15,49 @@ namespace Tests
             0 = new EmployeeViewModel(null);
         }
 
+        [TestMethod]
+        public void SalaryColorIsRedByDefaultTest()
+        {
+            Assert.AreEqual("red", o.SalaryColor);
+        }
+
+        [TestMethod]
+        public void SalaryColorIsRedIfSetColorArgumentIsNullTest()
+        {
+            o.setColor(null);
+            Assert.AreEqual("red", o.SalayColor);
+        }
+        [TestMethod]
+        public void SalaryColorIsYellowForHighSalariesTest()
+        {
+            o.setColor(new Employee(null, null, 15001));
+            Assert.AreEqual("yellow", o.SalaryColor);
+        }
+        [TestMethod]
+        public void SalaryColorIsGreenForSmallSalariesTest()
+        {
+            o.setColor(new Employee(null, null, 15000));
+            Assert.AreEqual("green", o.SalaryColor);
+        }
+        [TestMethod]
+        public void SalaryIsZeroByDefaultTest()
+        {
+            Assert.AreEqual(0.ToString("C"), o.Salary);
+        }
+        [TestMethod]
+        public void SalaryTest()
+        {
+            o.setSalary(new Employee(null, null, 100));
+            Assert.AreEqual(100.ToString("C"), o.Salary);
+        }
+        [TestMethod]
+        public void EmployeeNameTest()
+        {
+            o.setName(new Employee("First", "Last", 0));
+            Assert.AreEqual("First Last", o.EmployeeName);
+        }
+
+
 
     }
 }
