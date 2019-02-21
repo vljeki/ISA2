ccd666daed63fb92490763c8dc637112cd361f13
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-21 20:03:23 +0200

    Creat Labor3

--- Labor/Controllers/TestController.cs
+++ Labor/Controllers/TestController.cs
@@ -12,5 +12,10 @@ namespace Labor.Controllers
         {
             return "Hello world!";
         }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
     }
 }
