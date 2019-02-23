b0251d15e3c169f96cf897864b0d7bfa3552d4f6
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-02-23 11:36:20 +0200

    Finish the Labor 14

--- Core/Employee.cs
+++ Core/Employee.cs
@@ -8,24 +8,20 @@ namespace Core
 {
     public class Employee
     {
-        private const string requiredField = "Required field";
-        private const string lenghtIsToBig = "Length should be less than 20 characters!!";
-
         public Employee()
         {
         }
         public Employee(string firstName,
-            string lastName = null, int salary = 0)
+            string lastName, int salary)
         {
-            FirstName = firstName?? string.Empty;
-            LastName = lastName?? string.Empty;
+            FirstName = firstName;
+            LastName = lastName;
             Salary = salary;
         }
         public int EmployeeId { get; set; }
-        [Required(ErrorMessage = requiredField)]
-        [StringLength(20, ErrorMessage = lenghtIsToBig)]
+        [NameValidation]
         public string FirstName { get; set; }
-        [StringLength(20, ErrorMessage = lenghtIsToBig)]
+        [NameValidation]
         public string LastName { get; set; }
         public int Salary { get; set; }
 
