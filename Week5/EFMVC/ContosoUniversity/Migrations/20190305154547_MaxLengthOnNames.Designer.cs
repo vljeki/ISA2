20569441947b0337572ae9d456817c18997d24d2
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-03-05 17:50:22 +0200

    Costumize the Data Model

--- Week5/EFMVC/ContosoUniversity/Migrations/20190305154547_MaxLengthOnNames.Designer.cs
+++ Week5/EFMVC/ContosoUniversity/Migrations/20190305154547_MaxLengthOnNames.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190305154547_MaxLengthOnNames")]
+    partial class MaxLengthOnNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -65,12 +67,9 @@ namespace ContosoUniversity.Migrations
 
                     b.Property<DateTime>("EnrollmentDate");
 
-                    b.Property<string>("FirstMidName")
-                        .HasColumnName("FirstName")
-                        .HasMaxLength(50);
+                    b.Property<string>("FirstMidName");
 
-                    b.Property<string>("LastName")
-                        .HasMaxLength(50);
+                    b.Property<string>("LastName");
 
                     b.HasKey("ID");
 
--- Week5/EFMVC/ContosoUniversity/Models/Student.cs
+++ Week5/EFMVC/ContosoUniversity/Models/Student.cs
@@ -10,24 +10,14 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
-
-        
+        [StringLength(50)]
         public string LastName { get; set; }
-
-      
+        [StringLength(50)]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
-
-        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-
-     
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
         public ICollection<Enrollment> Enrollments { get; set; }
   
     }
