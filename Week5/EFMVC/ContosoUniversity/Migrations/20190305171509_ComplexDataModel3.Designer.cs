b2c026ec177c201c188ec8c9ed20c7295c974740
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-03-05 19:20:08 +0200

    Can't update SQL database 3

--- Week5/EFMVC/ContosoUniversity/Migrations/20190305171509_ComplexDataModel3.Designer.cs
+++ Week5/EFMVC/ContosoUniversity/Migrations/20190305171509_ComplexDataModel3.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190305171509_ComplexDataModel3")]
+    partial class ComplexDataModel3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
