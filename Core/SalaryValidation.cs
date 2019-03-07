9298e81a4953ae67a0b8ea8feef033dc811b3438
Viljam Puusep <viljam.puusep@ttu.ee>
2019-03-07 13:20:54 +0200

    Nimeruumid parandatud. Viited tehtud õigeks.

--- Core/SalaryValidation.cs
+++ Core/SalaryValidation.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.Data.Edm.Validation;
+﻿
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
-using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
 
 namespace Core
 {
