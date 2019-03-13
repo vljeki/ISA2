02a6807bb73e6f7e6b6ff87705b8ac45ed071016
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-03-13 23:13:20 +0200

    Update the Delete page

--- Week7/ContosoUniversity2/Pages/Departments/Delete.cshtml.cs
+++ Week7/ContosoUniversity2/Pages/Departments/Delete.cshtml.cs
@@ -20,40 +20,51 @@ namespace ContosoUniversity2.Pages.Departments
 
         [BindProperty]
         public Department Department { get; set; }
+        public string ConcurrencyErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int id, bool? concurrencyError)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             Department = await _context.Departments
-                .Include(d => d.Administrator).FirstOrDefaultAsync(m => m.DepartmentID == id);
+                .Include(d => d.Administrator)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.DepartmentID == id);
 
             if (Department == null)
             {
                 return NotFound();
             }
+
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ConcurrencyErrorMessage = "The record you attempted to delete "
+                  + "was modified by another user after you selected delete. "
+                  + "The delete operation was canceled and the current values in the "
+                  + "database have been displayed. If you still want to delete this "
+                  + "record, click the Delete button again.";
+            }
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
+                if (await _context.Departments.AnyAsync(
+                    m => m.DepartmentID == id))
+                {
+                    // Department.rowVersion value is from when the entity
+                    // was fetched. If it doesn't match the DB, a
+                    // DbUpdateConcurrencyException exception is thrown.
+                    _context.Departments.Remove(Department);
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToPage("./Index");
             }
-
-            Department = await _context.Departments.FindAsync(id);
-
-            if (Department != null)
+            catch (DbUpdateConcurrencyException)
             {
-                _context.Departments.Remove(Department);
-                await _context.SaveChangesAsync();
+                return RedirectToPage("./Delete",
+                    new { concurrencyError = true, id = id });
             }
-
-            return RedirectToPage("./Index");
         }
     }
 }
