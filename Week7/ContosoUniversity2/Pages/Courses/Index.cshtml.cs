d4944679acd220d32c98f2b0ac3344770354eaa8
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-03-13 21:30:00 +0200

    Create a Course page that displays department name

---
+++ Week7/ContosoUniversity2/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity2.Models;
+
+namespace ContosoUniversity2.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity2.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity2.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
