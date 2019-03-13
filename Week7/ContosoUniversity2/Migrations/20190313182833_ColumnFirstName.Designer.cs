a1857e224607e1c751ddf3701aaebfa9ec02c559
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-03-13 20:42:24 +0200

    Student entity update

--- Week7/ContosoUniversity2/Migrations/20190313182833_ColumnFirstName.Designer.cs
+++ Week7/ContosoUniversity2/Migrations/20190313182833_ColumnFirstName.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity2.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity2.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190313182833_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
