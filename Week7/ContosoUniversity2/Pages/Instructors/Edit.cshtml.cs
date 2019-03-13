7c75111ebd120267f8e6b7e1f79685709e3c52a3
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-03-13 21:44:39 +0200

    Create an Instructors page that shows Courses and Enrollments

---
+++ Week7/ContosoUniversity2/Pages/Instructors/Edit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity2.Models;
+
+namespace ContosoUniversity2.Pages.Instructors
+{
+    public class EditModel : PageModel
+    {
+        private readonly ContosoUniversity2.Models.SchoolContext _context;
+
+        public EditModel(ContosoUniversity2.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Instructor Instructor { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Instructor == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Instructor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InstructorExists(Instructor.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool InstructorExists(int id)
+        {
+            return _context.Instructors.Any(e => e.ID == id);
+        }
+    }
+}
